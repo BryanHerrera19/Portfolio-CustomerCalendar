a8d00a4da6e193818efef0ce5d08b25190dceaa0
Kevin Lin <k_lin30@u.pacific.edu>
2023-04-01 16:01:12 -0700

    fixed import error in pylint

--- src/main.py
+++ src/main.py
@@ -1,22 +1,18 @@
-import tkinter as tk #GUI
-from tkcalendar import * #adding calender function and stylings
-
-
+import tkinter as tk
+import tkcalendar
 
 win = tk.Tk()
 win.title("Magneton")
 
-cal = Calendar(win, font="Arial 14", selectmode="day",
-                    locale="en_US", year=2023, month=4, day=1,
-                    showweeknumbers = False,
-                    headersbackground ="white",
-                    background = "white",
-                    foreground = "black",  #font colour
-                    selectbackground = "skyblue",
-                    normalbackground = "white",
-                    weekendbackground = "lightgray")
-
-
+cal = tkcalendar.Calendar(win, font="Arial 14", selectmode="day",
+                          locale="en_US", year=2023, month=4, day=1,
+                          showweeknumbers=False,
+                          headersbackground="white",
+                          background="white",
+                          foreground="black",  # font colour
+                          selectbackground="skyblue",
+                          normalbackground="white",
+                          weekendbackground="lightgray")
 
 cal.pack(fill="both", expand=True)
 
