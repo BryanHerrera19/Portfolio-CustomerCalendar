2ea94bda1dbd45ee63675436aae2818fef650571
Kevin Lin <k_lin30@u.pacific.edu>
2023-04-01 14:22:02 -0700

    added default calender + formatted day/month/year in header

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
 from datetime import datetime
 import tkinter as tk
 from tkcalendar import Calendar
+
+
+
+win = tk.Tk()
+
+calendar = Calendar(win, selectmode="day",year= 2021, month=3, day=3,
+                          showweeknumbers = False)
+
+
+
+calendar.pack()
+
+win.mainloop()
