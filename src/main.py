c477c44d443ecdfad569e200856eded120c63a99
Bryan <b_herrera6@u.pacific.edu>
2023-04-02 14:56:12 -0700

    Added create event button and pop up window

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkcalendar import Calendar
 
 win = tk.Tk()
+win.geometry('1280x720')
 win.title("Magneton")
 
 cal = Calendar(win, font="Arial 14", selectmode="day",
@@ -15,6 +16,19 @@ cal = Calendar(win, font="Arial 14", selectmode="day",
                selectbackground="skyblue",
                normalbackground="white",
                weekendbackground="lightgray")
+
+cal.pack(pady=20)
+
+def create_event():
+    top = tk.Toplevel(win)
+    top.geometry('640x360')
+    top.title("Creating Event")
+    tk.Label(top, text="Enter Event Details", font="Arial 14")
+
+tk.Button(win, text="Create New Event",
+       command=create_event).pack(pady=20)
+
+date = tk.Label(win, text="")
 
 cal.pack(fill="both", expand=True)
 
