b5c825564e6d0408cffa1f60a1d2864005c98a2e
Bryan <b_herrera6@u.pacific.edu>
2023-04-02 15:37:09 -0700

    added drop down for setting date and labeled popup

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 import tkinter as tk
 
 from tkcalendar import Calendar
+from tkcalendar import DateEntry
 
 win = tk.Tk()
 win.geometry('1280x720')
 win.title("Magneton")
 
 cal = Calendar(win, font="Arial 14", selectmode="day",
-               locale="en_US", year=2023, month=4, day=1,
+               locale="en_US",
                showweeknumbers=False,
                headersbackground="white",
                background="white",
@@ -24,7 +25,15 @@ def create_event():
     top = tk.Toplevel(win)
     top.geometry('640x360')
     top.title("Creating Event")
-    tk.Label(top, text="Enter Event Details", font="Arial 14")
+
+    #Label Declerations
+    main_label = tk.Label(top, text="Enter Event Details", font="Arial 14")
+    tempCal = DateEntry(top, selectmode='day',showweeknumbers=False, pady=10)
+    #Label Positioning
+    main_label.pack(pady=5)
+    tempCal.pack(pady=10)
+    #Drop down date setup
+    tempCal.set_date(cal.get_date())
 
 tk.Button(win, text="Create New Event",
        command=create_event).pack(pady=20)
