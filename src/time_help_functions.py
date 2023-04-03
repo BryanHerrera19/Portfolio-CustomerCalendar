14e9b80d7536a36a34a28229284521178628760c
marissam27 <m_morones@u.pacific.edu>
2023-04-03 11:29:25 -0700

    Edited month lists into a single list

--- src/time_help_functions.py
+++ src/time_help_functions.py
@@ -2,8 +2,8 @@
 def convert_date(enter):
     """ Converts datetime to readable date"""
     new_input = str(enter)
-    months = ["January", "February", "March", "April", "May", "June", "July"]
-    months.append( ["August", "September", "October", "November", "December"])
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+              "November", "December"]
     output = ""
     day, month, year = "", "", ""
     date_index = 0
@@ -13,14 +13,14 @@ def convert_date(enter):
             date_index += 1
         else:
             date[date_index] += character
-    date[1]= str(int(date[1]) - 1)
+    date[1] = str(int(date[1]) - 1)
     month1, day1, year1 = months[int(date[1])], date[2], date[0]
     output = month1 + " " + day1 + ", " + year1
     return output
 def convert_hour(enter):
-    """Converts Time to a 12 hour system"""
+    """Converts Time to a 12-hour system"""
     new_input = int(enter)
     if new_input > 12:
-        output = new_input -12
+        output = new_input - 12
         return output
     return new_input
