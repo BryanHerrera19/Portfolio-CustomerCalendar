4a037e79198bee5b1d65d88ab438e95f76bea39b
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-04-16 11:59:44 -0700

    Fixing Pylint Errors

--- src/Unit_Tests.py
+++ src/Unit_Tests.py
@@ -9,13 +9,14 @@ from Event_Info import Event as eventInfo
 
 
 class Test(unittest.TestCase):
-    """Test Cases for Checking if Calendar Successfully saves Events"""
+    """Test Cases for Calendar"""
+    # Test Cases for Checking if Calendar Successfully saves Events"""
     def test_Save_A_New_Event_Should_Return_True(self):
         """Test Case should return True"""
         event_list = [eventInfo(date.today(), "1:00PM", "2:00PM", "Class", "Online", "ARTS")]
         self.assertEqual(f"{event_list[0].getName()}", "Class")
 
-    """Test Cases to Check if Timer Input Checking is working Properly"""
+    # Test Cases to Check if Timer Input Checking is working Properly"""
     def test_Timer_with_Letter_Input(self):
         """Test Case Should Run Properly"""
         hourString, minuteString, secondString = "1","45", "A0" #Inputted as 1:45:A0
