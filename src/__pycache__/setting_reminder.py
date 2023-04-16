dbe106e6aef4347a6e997f0d8196e5a61cd46f85
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-04-16 11:08:22 -0700

    Fixing Pylint Errors

--- src/__pycache__/setting_reminder.py
+++ src/__pycache__/setting_reminder.py
@@ -1,10 +1,11 @@
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
 from datetime import date
 
 def upcoming_event(event):
     """Not Complete, will need to include Time function"""
-    eventDay == str(event.getDay())
-    today == str(date.today())
+    eventDay = str(event.getDay())
+    today = str(date.today())
     if eventDay == today:
         return True
     return False
-
