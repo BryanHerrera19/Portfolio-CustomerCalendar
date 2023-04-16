57260ae05c9696b4a2e29c9461928e9783e19bf3
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-04-16 11:58:14 -0700

    Added Timer User Test Cases

--- src/Unit_Tests.py
+++ src/Unit_Tests.py
@@ -15,6 +15,23 @@ class Test(unittest.TestCase):
         event_list = [eventInfo(date.today(), "1:00PM", "2:00PM", "Class", "Online", "ARTS")]
         self.assertEqual(f"{event_list[0].getName()}", "Class")
 
+    """Test Cases to Check if Timer Input Checking is working Properly"""
+    def test_Timer_with_Letter_Input(self):
+        """Test Case Should Run Properly"""
+        hourString, minuteString, secondString = "1","45", "A0" #Inputted as 1:45:A0
+        with self.assertRaises(ValueError):
+            t = int(hourString)*3600 + int(minuteString)*60 + int(secondString)
+    def test_Timer_with_Missing_Input(self):
+        """Test Case Should Run Properly"""
+        hourString, minuteString, secondString = "","45", "23" #Inputted as :45:23
+        with self.assertRaises(ValueError):
+            t = int(hourString)*3600 + int(minuteString)*60 + int(secondString)
+    def test_Timer_with_Symbol_Input(self):
+        """Test Case Should Run Properly"""
+        hourString, minuteString, secondString = "1","%35", "23" #Inputted as 1:%35:23
+        with self.assertRaises(ValueError):
+            t = int(hourString)*3600 + int(minuteString)*60 + int(secondString)
+
 
 if __name__ == '__main__':
     unittest.main()
