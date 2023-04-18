f58a5c9fa79dd6570199545c686e35635b1daf27
Kevin Lin <k_lin30@u.pacific.edu>
2023-04-18 03:14:26 -0700

    readded missing file

---
+++ src/time_1.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-module-docstring
+#from datetime import date
+
+from datetime import datetime
+
+import time_help_functions as of
+
+def get_time():
+    """Grabs the current time in readable format"""
+    now = datetime.now()
+    hour = now.strftime("%H")
+    minute = now.strftime("%M")
+    second = now.strftime("%S")
+    ante_post = now.strftime("%p")
+    hour = of.convert_hour(hour)
+    current_time = f"{hour}:{minute}:{second} {ante_post}"
+    return current_time
